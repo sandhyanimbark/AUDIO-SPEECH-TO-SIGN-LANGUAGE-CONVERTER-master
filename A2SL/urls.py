dc8b0cfc182efd3a7eef30f0bc25aa186ec2242c
tispn <64691616+sandhyanimbark@users.noreply.github.com>
2022-03-15 08:28:24 +0530

    Update urls.py

--- A2SL/urls.py
+++ A2SL/urls.py
@@ -17,9 +17,7 @@ from unicodedata import name
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
-from . import views
-from django.views.static import serve
-from django.conf.urls import url
+
 
 
 urlpatterns = [
@@ -33,8 +31,7 @@ urlpatterns = [
     path('',views.home_view,name='home'),
     path('animation/',views.animation_view,name='animation'),
     path('loc/',views.location,name='location'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    
     
 ]
-urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
