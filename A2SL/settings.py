2000b4e7fcb6d43f48251b244f8a7af7189aeac7
tispn <64691616+sandhyanimbark@users.noreply.github.com>
2022-03-15 07:44:26 +0530

    Update settings.py

--- A2SL/settings.py
+++ A2SL/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '3k7=!d39#4@_&5a6to&4=_=j(c^v0(vv91cj5+9e8+d4&+01jb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','csv123.herokuapp.com']
 
 
 # Application definition
